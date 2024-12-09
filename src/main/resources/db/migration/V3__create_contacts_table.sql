c2e7d71088f31c3603ef1218fb5304a2957a7a28
Patrick Dumdum <patrickedumdum@gmail.com>
2024-12-09 15:02:58 +0000

    changed migration name, changed datatype

--- src/main/resources/db/migration/V3__create_contacts_table.sql
+++ src/main/resources/db/migration/V3__create_contacts_table.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS contacts;
 
 CREATE TABLE contacts (
-    user_id1 bigserial PRIMARY KEY,
-    user_id2 INT,
+    user_id1 BIGSERIAL PRIMARY KEY,
+    user_id2 BIGSERIAL,
       CONSTRAINT fk_user
         FOREIGN KEY (user_id2)
         REFERENCES users(id)
