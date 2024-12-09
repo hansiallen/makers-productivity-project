2a78494a73ed47da9c6685dd312472f788ec04db
Alexandre Monoyioudis <alexandremonoyioudis@gmail.com>
2024-12-09 17:26:08 +0000

    bugfix V6__add_event_atendees_table.sql

--- src/main/resources/db/migration/V6__add_event_atendees_table.sql
+++ src/main/resources/db/migration/V6__add_event_atendees_table.sql
@@ -3,12 +3,12 @@ DROP TABLE IF EXISTS event_attendees;
 CREATE TABLE event_attendees (
     id BIGSERIAL PRIMARY KEY,
     event_id BIGSERIAL,
-          CONSTRAINT fk_event_id
+          CONSTRAINT fk_event
             FOREIGN KEY (event_id)
             REFERENCES events(id)
             ON DELETE CASCADE,
     attendee_id BIGSERIAL,
-      CONSTRAINT fk_attendee_id
+      CONSTRAINT fk_attendee
         FOREIGN KEY (attendee_id)
         REFERENCES user_profiles(id)
         ON DELETE CASCADE,
