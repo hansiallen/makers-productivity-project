8f8066849638ed32d2c4e1c0a3cbde6dfe284feb
Patrick Dumdum <patrickedumdum@gmail.com>
2024-12-10 11:48:06 +0000

    get current user functionality

--- src/main/java/com/example/productivity/controller/CurrentUserController.java
+++ src/main/java/com/example/productivity/controller/CurrentUserController.java
@@ -18,31 +18,34 @@ public class CurrentUserController {
 
     @Autowired
     UserRepository userRepository;
-    private Map<String, Object> userDetails;
+    private String email;
+    private Long id;
+    private String auth0id;
 
     @GetMapping("users/after-login")
     public RedirectView handleLogin(){
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         DefaultOidcUser principal = (DefaultOidcUser) auth.getPrincipal();
-        this.userDetails = principal.getAttributes();
+        Map<String, Object> userDetails = principal.getAttributes();
+
+        this.email = userDetails.get("email").toString();
+        this.auth0id = userDetails.get("sub").toString();
 
         User user = userRepository.findByAuth0Id(getAuth0id());
         if (user == null){
-
             user = new User();
             user.setAuth0Id(getAuth0id());
             user.setEmail(getEmail());
             userRepository.save(user);
         }
+
+        this.id = user.getId();
         return new RedirectView("/");
     }
 
-    public String getEmail() {
-        return this.userDetails.get("email").toString();
-    }
-    public String getAuth0id(){
-        return this.userDetails.get("sub").toString();
-    }
+    public String getEmail() {return this.email;}
+    public Long getId(){return this.id;}
+    public String getAuth0id(){return this.auth0id;}
 
 }
