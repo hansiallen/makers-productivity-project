8fe62a2ab8d2eafaa333c274df282ba1f42d0ddd
Patrick Dumdum <patrickedumdum@gmail.com>
2024-12-11 17:16:49 +0000

    patch 2

--- src/main/java/com/example/productivity/repository/CustomFieldRepository.java
+++ src/main/java/com/example/productivity/repository/CustomFieldRepository.java
@@ -3,5 +3,8 @@ package com.example.productivity.repository;
 import com.example.productivity.model.CustomField;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CustomFieldRepository extends CrudRepository<CustomField,Long> {
+    List<CustomField> findByUserId(Long id);
 }
