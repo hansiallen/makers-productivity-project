9957db18d98194451502f6039e1cf5abbcc493a1
Isaac Spiers <isaacspiers7@gmail.com>
2024-12-11 10:16:29 +0000

    fix migrations

--- src/main/resources/db/migration/V3__add_user_links_table.sql
+++ src/main/resources/db/migration/V3__add_user_links_table.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS user_links;
 
 CREATE TABLE user_links (
     id bigserial PRIMARY KEY,
-    user_profile_id bigint NOT NULL,
+    user_id bigint NOT NULL,
     website_link_name VARCHAR(255),
     website_link_url VARCHAR(255),
-    FOREIGN KEY (user_profile_id) REFERENCES user_profiles(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES user_profiles(id) ON DELETE CASCADE
 );
