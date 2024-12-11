bb24b9b46e82913e5cb1896c2e7cadf86f282400
Isaac Spiers <isaacspiers7@gmail.com>
2024-12-11 16:23:27 +0000

    made contacts page dynamic

--- src/main/java/com/example/productivity/controller/HomeController.java
+++ src/main/java/com/example/productivity/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.productivity.controller;
 
+import com.example.productivity.model.UserProfile;
+import com.example.productivity.repository.UserProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,9 +10,14 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class HomeController {
 
+    @Autowired
+    UserProfileRepository userProfileRepository;
+
     @GetMapping("/")
     public ModelAndView userProfile() {
         ModelAndView modelAndView = new ModelAndView("/page/contacts.html");
+        Iterable<UserProfile> contacts = userProfileRepository.findAll();
+        modelAndView.addObject("contacts", contacts);
         return modelAndView;
     }
 }
