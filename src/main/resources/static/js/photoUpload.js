3526593be9a3b6b588020ae14a7375497b5b145e
Jess Pates <jesspates@hotmail.co.uk>
2024-12-12 21:40:59 +0000

    profile photo form integrated

---
+++ src/main/resources/static/js/photoUpload.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const fileInput = document.querySelector('#profilePhoto');
+    const photoTick = document.querySelector('.photo-tick');
+
+    fileInput.addEventListener('change', () => {
+        if (fileInput.files && fileInput.files.length > 0) {
+          photoTick.classList.remove('hidden');
+        } else {
+          photoTick.classList.add('hidden');
+        }
+    });
+});
