e78c41ab4e2aba10d75da173abf15ed95e83e5c7
Jess Pates <jesspates@hotmail.co.uk>
2024-12-12 12:26:08 +0000

    favourite contacts added to homepage, update to contacts db table

--- src/main/java/com/example/productivity/controller/HomeController.java
+++ src/main/java/com/example/productivity/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.example.productivity.controller;
 
+import com.example.productivity.model.Contact;
 import com.example.productivity.model.Event;
+import com.example.productivity.model.UserProfile;
+import com.example.productivity.repository.ContactRepository;
 import com.example.productivity.repository.EventRepository;
+import com.example.productivity.repository.UserProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,6 +18,12 @@ import java.util.List;
 public class HomeController {
     @Autowired
     EventRepository eventRepository;
+    @Autowired
+    ContactRepository contactRepository;
+    @Autowired
+    CurrentUserController currentUser;
+    @Autowired
+    UserProfileRepository userProfileRepository;
 
     @GetMapping("/")
     public ModelAndView userProfile() {
@@ -24,7 +35,10 @@ public class HomeController {
     public ModelAndView userHome() {
         ModelAndView modelAndView = new ModelAndView("/page/home.html");
         List<Event> upcomingEvents = eventRepository.findNextUpcomingEventsNative(3);
+        List<Long> favouriteContactIds = contactRepository.findFavouritesUserIdsByUser1Id(currentUser.getCurrentUser().getId());
+        List<UserProfile> favouriteContactsProfiles = userProfileRepository.findAllById(favouriteContactIds);
         modelAndView.addObject("upcomingEvents", upcomingEvents);
+        modelAndView.addObject("favouriteContacts", favouriteContactsProfiles);
         return modelAndView;
     }
 }
