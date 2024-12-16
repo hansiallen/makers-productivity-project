fc636573d9df23c56ed15a52f686c08356b92ec4
patrick-ed <131492545+patrick-ed@users.noreply.github.com>
2024-12-16 13:57:52 +0000

    Revert "current user fix"

--- src/main/java/com/example/productivity/controller/CurrentUserController.java
+++ src/main/java/com/example/productivity/controller/CurrentUserController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,37 +18,42 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Map;
 
-@Controller
+@RestController
 public class CurrentUserController {
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
-    private UserProfileRepository userProfileRepository;
+    UserRepository userRepository;
 
     @Autowired
-    private CloudinaryService cloudinaryService;
+    UserProfileRepository userProfileRepository;
+    private final User currentUser = new User();
+
+    @Autowired
+    CloudinaryService cloudinaryService;
 
     @GetMapping("users/after-login")
-    public RedirectView handleLogin() {
+    public RedirectView handleLogin(){
+
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         DefaultOidcUser principal = (DefaultOidcUser) auth.getPrincipal();
         Map<String, Object> userDetails = principal.getAttributes();
+        System.out.println(userDetails);
 
-        String email = userDetails.get("email").toString();
-        String auth0Id = userDetails.get("sub").toString();
+        this.currentUser.setEmail(userDetails.get("email").toString());
+        this.currentUser.setAuth0Id(userDetails.get("sub").toString());
 
-
-        User user = userRepository.findByAuth0Id(auth0Id);
-        if (user == null) {
+        User user = userRepository.findByAuth0Id(currentUser.getAuth0Id());
+        if (user == null){
             user = new User();
-            user.setAuth0Id(auth0Id);
-            user.setEmail(email);
+            user.setAuth0Id(currentUser.getAuth0Id());
+            user.setEmail(currentUser.getEmail());
             userRepository.save(user);
         }
 
+        this.currentUser.setId(user.getId());
+
         UserProfile userProfile = userProfileRepository.findByUserId(user.getId());
-        if (userProfile == null) {
+        if (userProfile == null){
             userProfile = new UserProfile();
             userProfile.setUserId(user.getId());
             userProfileRepository.save(userProfile);
@@ -59,10 +63,5 @@ public class CurrentUserController {
         return new RedirectView("/");
     }
 
-    public User getCurrentUser() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        DefaultOidcUser principal = (DefaultOidcUser) auth.getPrincipal();
-        String auth0Id = principal.getAttributes().get("sub").toString();
-        return userRepository.findByAuth0Id(auth0Id);
-    }
+    public User getCurrentUser(){return this.currentUser;}
 }
