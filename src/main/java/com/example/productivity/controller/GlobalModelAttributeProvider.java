43506c44ee4ab5c0078f86aba1a0684074fce4b0
Patrick Dumdum <patrickedumdum@gmail.com>
2024-12-16 11:24:41 +0000

    added global model attribute provider to get details of current user in any template

---
+++ src/main/java/com/example/productivity/controller/GlobalModelAttributeProvider.java
@@ -0,0 +1,22 @@
+package com.example.productivity.controller;
+
+
+import com.example.productivity.model.User;
+import com.example.productivity.model.UserProfile;
+import com.example.productivity.repository.UserProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalModelAttributeProvider {
+    @Autowired
+    private CurrentUserController currentUser;
+    @Autowired
+    private UserProfileRepository userProfileRepository;
+
+    @ModelAttribute("currentUser") //Adds the attribute current user to every template
+    public UserProfile addCurrentUserToModel() {
+        return userProfileRepository.findByUserId(currentUser.getCurrentUser().getId());
+    }
+}
