e2f9ce587e67b354aab28c81465a3c75c92a0111
Johannes Allen <hansiallen@gmail.com>
2024-12-17 14:07:22 +0000

    Made app installable

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -224,3 +224,13 @@ function loadScript(url) {
     };
 }
 
+// Register service worker for installability purposes
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/js/sw.js')
+    .then((registration) => {
+        console.log('Service Worker registered');
+    })
+    .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+    });
+}
