212f071cadcb77c6d9751a34726f2e96fcc3a05d
Alexandre Monoyioudis <alexandremonoyioudis@gmail.com>
2024-12-18 15:44:27 +0000

    fixes minor issue

--- src/main/java/com/example/productivity/repository/EventRepository.java
+++ src/main/java/com/example/productivity/repository/EventRepository.java
@@ -21,11 +21,16 @@ public interface EventRepository extends CrudRepository<Event, Long> {
     List<Event> findNextUpcomingEvents(int limit, Long userId);
 
     @Query(value = """
-        SELECT e.id, e.date, e.start_time, e.end_time, e.title, e.description, e.user_id, e.is_cancelled
-        FROM events e, event_attendees a
-        WHERE (e.date >= :earliest)
-           and (e.date < :latest) and ((:userId = e.user_id) or (a.attendee_id = :userId and a.event_id = e.id))
-        ORDER BY e.date ASC, e.start_time ASC
+               SELECT e.id, e.date, e.start_time, e.end_time, e.title, e.description, e.user_id, e.is_cancelled
+                       FROM events e
+                       LEFT JOIN event_attendees a ON a.event_id = e.id
+                       WHERE e.date >= :earliest
+                         AND e.date < :latest
+                         AND (
+                             e.user_id = :userId\s
+                             OR (a.attendee_id = :userId)
+                         )
+                       ORDER BY e.date ASC, e.start_time ASC
     """, nativeQuery = true)
     List<Event> findEventsInTimePeriodForUser(Long userId, LocalDate earliest, LocalDate latest);
 }
